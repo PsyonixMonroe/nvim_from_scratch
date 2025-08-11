37776452d7825cb19261f27a8ca9bdad89b36d00
Matt Monroe <matt.monroe@psyonix.com>
2025-08-11 10:21:20 -0600

    Add verse for work machine

---
+++ lua/machines/psyonix/plugins/verse.lua
@@ -0,0 +1,25 @@
+return {
+    "https://github.ol.epicgames.net/matt-monroe/nvim-verse",
+    -- dir = "~/SideProjects/Verse/nvim-verse",
+    name = "nvim-verse",
+    ft = { "verse" },
+    dependencies = {
+        "rcarriga/nvim-notify",
+    },
+    opts = {
+        tsv_path = "~/SideProjects/tree-sitter-verse",
+        lsp_config = {
+            path = "verse-lsp",
+            uefn_path = "/mnt/c/Users/Matt/AppData/Local/UnrealEditorFortnite",
+        },
+        notifications = {
+            compile = true,
+            push = true,
+            build = false,
+            log = false,
+            can_push = false,
+            debug = false,
+        },
+    },
+
+}
