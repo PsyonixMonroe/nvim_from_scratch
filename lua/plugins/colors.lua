e0801d8b20946b86b096c0b24c2ca64bcc89fe3a
Matt Monroe <matt.monroe@psyonix.com>
2025-08-12 13:01:23 -0600

    trim color plugins and setup rainbow brackets

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,12 +1,5 @@
 return {
-    { "blazkowolf/gruber-darker.nvim", lazy = false },
-    { "ray-x/aurora",                  lazy = false },
-    { "ribru17/bamboo.nvim",           lazy = false },
-    { "xero/miasma.nvim",              lazy = false },
-    { "cocopon/iceberg.vim",           lazy = false },
-    { "p00f/nvim-ts-rainbow",          lazy = false }, -- Colorize matching brackets
-    { "folke/tokyonight.nvim",         lazy = false },
-    { "vague2k/vague.nvim",            lazy = false },
+    { "vague2k/vague.nvim", lazy = false },
     {
         "AstroNvim/astrotheme",
         priority = 1000, -- Make sure to load this before all the other start plugins.
@@ -14,8 +7,18 @@ return {
         lazy = false,
     },
     {
-        "ntk148v/habamax.nvim",
-        dependencies = { "rktjmp/lush.nvim" },
+        "hiphish/rainbow-delimiters.nvim",
         lazy = false,
-    },
+    }
+    -- { "blazkowolf/gruber-darker.nvim", lazy = false },
+    -- { "ray-x/aurora",                  lazy = false },
+    -- { "ribru17/bamboo.nvim",           lazy = false },
+    -- { "xero/miasma.nvim",              lazy = false },
+    -- { "cocopon/iceberg.vim",           lazy = false },
+    -- { "folke/tokyonight.nvim",         lazy = false },
+    -- {
+    --      "ntk148v/habamax.nvim",
+    --      dependencies = { "rktjmp/lush.nvim" },
+    --      lazy = false,
+    -- },
 }
