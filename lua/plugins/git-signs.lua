ae17378308d9119975674aa774e811b22a2d024d
Matt monroe <matt.monroe@psyonix.com>
2026-01-09 17:32:41 -0700

    Add keybinds for jumping to the next modification in the file

--- lua/plugins/git-signs.lua
+++ lua/plugins/git-signs.lua
@@ -1,3 +1,15 @@
 return {
     "lewis6991/gitsigns.nvim",
+    opts = {
+        on_attach = function(bufnr)
+            local gs = require('gitsigns')
+            local map = function(mode, l, r, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, l, r, opts)
+            end
+            map('n', '<leader>gn', gs.next_hunk, { desc = "Next git hunk" })
+            map('n', '<leader>gN', gs.prev_hunk, { desc = "Prev git hunk" })
+        end
+    }
 }
